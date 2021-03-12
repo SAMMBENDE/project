61bdfaf6a2d5b8bc40f98b6b609fbdfb57d35f54
SAMMBENDE <51370083+SAMMBENDE@users.noreply.github.com>
2021-03-12 03:41:15 +0100

    via desktop

---
+++ backend/data/products.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    name: "Gombo Royale",
+    imageUrl:
+      "https://niammy.com/img/public/990/201903/1553301446_29522.jpg",
+    description:
+      "Légumes Camerounaises préparé avec les arachides et crevettes, viande, poisson fumé etc",
+    price: 15,
+    countInStock: 15,
+  },
+  {
+    name: "Ndolé",
+    imageUrl:
+      "https://www.people237.com/wp-content/uploads/2019/02/ndol%C3%A8.jpg",
+    description:
+      "Légumes Camerounaises préparé avec les arachides et crevettes, viande, poisson fumé etc",
+    price: 20,
+    countInStock: 10,
+  },
+  {
+    name: "Garri & Eru",
+    imageUrl:
+      "https://pbs.twimg.com/media/EDO-LMGXUAENc8O?format=jpg&name=small",
+    description:
+      "Un mélange de légumes okok et les épinards à petites feuilles. Plat très prisé et surtout savoureux",
+    price: 15,
+    countInStock: 5,
+  },
+  {
+    name: "Porc Braisé",
+    imageUrl:
+      "https://images.unsplash.com/photo-1570466199040-4fe6e9da19dd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80",
+    description:
+      "Acompagnement aux choix. Riz, Plantain tapé, Bobolo, Miondo",
+    price: 20,
+    countInStock: 25,
+  },
+  {
+    name: "Poisson Braisé",
+    imageUrl:
+      "https://images.unsplash.com/photo-1598514982205-f36b96d1e8d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    description:
+      "Acompagnement aux choix. Riz, Plantain tapé, Bobolo, Miondo, Aloko",
+    price: 10,
+    countInStock: 44,
+  },
+  {
+    name: "Brochettes de Boeuf",
+    imageUrl:
+      "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80",
+    description:
+      "Acompagnement aux choix. Riz, Plantain tapé, Bobolo, Miondo, Aloko",
+    price: 15,
+    countInStock: 10,
+  },
+];
+
+module.exports = products;
---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllProducts,
+  getProductById,
+} = require("../controller/productControllers");
+
+// GET all products from db
+// GET /api/products
+// Public
+
+router.get("/", getAllProducts);
+
+// GET a product by id from db
+// GET /api/products/:id
+// Public
+
+router.get("/:id", getProductById);
+
+module.exports = router;
