fc025b98a39505251bfc7620a38c18c5da18a3e0
SAMMBENDE <51370083+SAMMBENDE@users.noreply.github.com>
2021-03-18 20:44:13 +0100

    fix 2

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -10,7 +10,7 @@ const Product = ({imageUrl, name, price, description, productid}) => {
             <div className="product__info">
                 <p className="info__name">{name}</p>
                 <p className="info__description">
-                {description.substring(0, 100)}...
+                {description.substring(0, 100)}...  
                 </p>
                 <p className="info__price">€{price}</p>
                 
