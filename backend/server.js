3d21eb9470711a6ef97c72f156cfe4bc9df95c80
SAMMBENDE <51370083+SAMMBENDE@users.noreply.github.com>
2021-04-21 16:48:36 +0200

    'again'

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ if(process.env.NODE_ENV ==='production') {
      app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
      });
+}else{
+   app.get('/', (req, res) => {
+      res.send('Api running')
+   })
 }
 
 const PORT = process.env.PORT || 5000
