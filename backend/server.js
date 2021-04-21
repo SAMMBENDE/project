a5a2edc841c16bbe136ac089ce5ecb226231eaf5
SAMMBENDE <51370083+SAMMBENDE@users.noreply.github.com>
2021-04-21 20:38:56 +0200

    fixes

--- backend/server.js
+++ backend/server.js
@@ -12,21 +12,22 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes);
 
-
-// Serve static assets if in production
+// Serve static assets if in production 
+// this if  else is important; It enables heroku server to serve my react app
 if(process.env.NODE_ENV ==='production') {
-     // Set a static folder
-     app.use(express.static('frontend/build'));
+
+     // Set a static folder This is a middleware
+     app.use(express.static(path.join(__dirname, 'frontend/build')));
 
      app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-     });
-}else{
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+     })
+} else {
    app.get('/', (req, res) => {
-      res.send('Api running')
-   })
+      res.send('Api running');
+   });
 }
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
