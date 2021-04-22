376179ea6c2cf5c1327dc9c822e399af689cc9ad
SAMMBENDE <51370083+SAMMBENDE@users.noreply.github.com>
2021-04-22 02:45:11 +0200

    fix

--- backend/server.js
+++ backend/server.js
@@ -12,20 +12,27 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes);
 
-// Serve static assets if in production
-if(process.env.NODE_ENV ==='production') {
+// // Serve static assets if in production
+// if(process.env.NODE_ENV ==='production') {
 
-     // Set a static folder This is a middleware
-     app.use(express.static('frontend/build'));
+//      // Set a static folder This is a middleware
+//      app.use(express.static('frontend/build'));
 
-     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-     });
-}else{
-   app.get('/', (req, res) => {
-      res.send('Api running')
-   });
-}
+//      app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+//      });
+// }else{
+//    app.get('/', (req, res) => {
+//       res.send('Api running')
+//    });
+// }
+process.env.NODE_ENV === 'production'
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+app.get('/*',  (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
+
 
 const PORT = process.env.PORT || 5000;
 
