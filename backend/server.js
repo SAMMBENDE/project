a77e18b2d83d5e9b108543f105f58a85f89042d3
SAMMBENDE <51370083+SAMMBENDE@users.noreply.github.com>
2021-04-22 02:11:46 +0200

    fixes

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,13 @@ if(process.env.NODE_ENV ==='production') {
 //    });
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
+app.get('/', (req, res) => {
+   res.send()
+});
 app.get('/*', (req, res) => {
    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
+
 }
 
 const PORT = process.env.PORT || 5000;
