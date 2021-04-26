0ab46499f2f33dba343208ab9afce558e28eb099
SAMMBENDE <51370083+SAMMBENDE@users.noreply.github.com>
2021-04-26 21:24:35 +0200

    rmoved hashrouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,15 +6,12 @@ import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <Provider
     store={store}>
-    <React.StrictMode>
-      <HashRouter>
-      <App />
-      </HashRouter>
+    <React.StrictMode>      
+      <App />      
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
